ddef4517e01e4eb0740976f36da510893f7d296d
Anne de Joly <anne.dejoly@edhec.com>
2021-11-03 23:42:56 +0000

    Add boilerplate

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const list = document.querySelector('#results');
+const form = document.querySelector('#search-movies');
+
+const fetchMovies = (keyword) => {
+  fetch(`http://www.omdbapi.com/?s=${keyword}&apikey=adf1f2d7`)
+    .then(response => response.json())
+    .then((data) => {
+      data.Search.forEach((result) => {
+        const movieTag = `<li>
+          <img src="${result.Poster}">
+          <p>${result.Title}</p>
+          </li>`;
+          list.insertAdjacentHTML("beforeend", movieTag);
+        });
+      });
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  list.innerHTML = "";
+  const input = document.querySelector('#keyword');
+  fetchMovies(input.value);
+});
+
+fetchMovies('Batman');
